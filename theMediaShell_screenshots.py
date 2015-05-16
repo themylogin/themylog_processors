842851dc1423eae3005b1d0f067c2410dd965351
themylogin <themylogin@gmail.com>
2015-05-16 18:06:31 +0600

    DVD/BluRay screenshots & take screenshots at the beginning of the movie

--- theMediaShell_screenshots.py
+++ theMediaShell_screenshots.py
@@ -13,8 +13,26 @@ import tempfile
 
 
 def process(record):
-    if record.application == "theMediaShell" and record.logger == "movie" and record.msg == "end":
+    if record.application == "theMediaShell" and record.logger == "movie" and record.msg in ["start", "end"]:
         filename = record.args["movie"].replace("/home/themylogin/Storage", "/media/storage")
+
+        dir = os.path.join("/home/themylogin/www/thelogin.ru/data/movies",
+                           os.path.relpath(filename, "/media/storage/Torrent/downloads")).encode("utf-8")
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        else:
+            if len(os.listdir(dir)) == 4:
+                return
+
+        if os.path.isdir(filename.encode("utf-8")):
+            if os.path.exists(os.path.join(filename, "BDMV").encode("utf-8")):
+                url = "br:///%s" % filename
+            elif os.path.exists(os.path.join(filename, "VIDEO_TS").encode("utf-8")):
+                url = "dvd:///%s" % filename
+            else:
+                raise Exception("%s is a directory" % filename)
+        else:
+            url = filename
 
         i = 1
         length = int(re.search(r"ID_LENGTH=([0-9]+)",
@@ -24,7 +42,7 @@ def process(record):
                                                         "-vo", "null",
                                                         "-frames", "0",
                                                         "-identify",
-                                                        filename],
+                                                        url.encode("utf-8")],
                                                        stderr=subprocess.STDOUT)).group(1))
         for ss in range(int(length / 5 + 1), length, int(length / 5 + 1)):
             tmp_dir = str(tempfile.mkdtemp())
@@ -36,7 +54,7 @@ def process(record):
                              "-vo", "jpeg:outdir=" + tmp_dir,
                              "-subcp", "enca:ru:cp1251",
                              "-frames", "50",
-                             filename])
+                             url.encode("utf-8")])
 
             max_brightness = -1
             max_brightness_image = None
@@ -47,11 +65,7 @@ def process(record):
                     max_brightness = brightness
                     max_brightness_image = image
 
-            dir = os.path.join("/home/themylogin/www/thelogin.ru/data/movies",
-                               os.path.relpath(filename, "/media/storage/Torrent/downloads"))
-            if not os.path.exists(dir):
-                os.makedirs(dir)
-            shutil.copy(max_brightness_image, os.path.join(dir, "%d.jpg" % i))
+            shutil.copy(max_brightness_image, os.path.join(dir, b"%d.jpg" % i))
 
             for image in glob.glob(os.path.join(tmp_dir, "0000*.jpg")):
                 os.unlink(image)
